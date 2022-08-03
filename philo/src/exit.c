b9c510b9c1eb7e6e7a2d1f3b5429caea369a8238
Archibald THIRION <athirion@e3r4p23.clusters.42paris.fr>
2022-08-03 18:22:26 +0200

    fix details

--- philo/src/exit.c
+++ philo/src/exit.c
@@ -6,7 +6,7 @@
 /*   By: athirion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 09:28:42 by athirion          #+#    #+#             */
-/*   Updated: 2022/07/31 09:28:43 by athirion         ###   ########.fr       */
+/*   Updated: 2022/08/03 18:19:38 by athirion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ void	ft_exit_philo(t_data *data, t_philo *philo)
 		i ++;
 	}
 	pthread_mutex_destroy(&(data->eat));
+	pthread_mutex_destroy(&(data->death));
 	pthread_mutex_destroy(&(data->log));
 }
